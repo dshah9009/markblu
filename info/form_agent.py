85e6c05f108d2f853e255f03352f1bf991efb0d1
Deepak kumar shah <deepakshahwal999@gmail.com>
2025-08-11 14:23:16 +0530

    update edit profile

--- info/form_agent.py
+++ info/form_agent.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models_agent import PropertyVideo
+from django.core.files.uploadedfile import UploadedFile
 
 
 from django.core.exceptions import ValidationError
@@ -12,33 +13,35 @@ class PropertyVideoForm(forms.ModelForm):
 
     def clean_video(self):
         video = self.cleaned_data.get('video', False)
-        if video:
-            valid_mime_types = valid_mime_types = [
-    'video/mp4',
-    'video/mpeg',
-    'video/quicktime',
-    'video/x-msvideo',
-    'video/x-matroska',
-    'video/webm',
-    'video/3gpp',
-    'video/3gpp2',
-    'video/ogg',
-    'video/x-flv',
-    'video/MP2T',
-    'application/x-mpegURL',
-    'video/avi',
-    'video/x-ms-wmv',
-    'video/x-ms-asf',
-    'video/vnd.dlna.mpeg-tts'
-]
-            if video.content_type not in valid_mime_types:
-                raise ValidationError('Unsupported file type. Please upload a video file (mp4, mov, avi).')
-            if video.size > 200 * 1024 * 1024:  # 200MB Limit
-                raise ValidationError('Please keep filesize under 200MB.')
-            return video
-        else:
-            raise ValidationError("Couldn't read uploaded file.")
 
+        # Validate only if a new video is being uploaded
+        if isinstance(video, UploadedFile):
+            valid_mime_types = [
+                'video/mp4',
+                'video/mpeg',
+                'video/quicktime',
+                'video/x-msvideo',
+                'video/x-matroska',
+                'video/webm',
+                'video/3gpp',
+                'video/3gpp2',
+                'video/ogg',
+                'video/x-flv',
+                'video/MP2T',
+                'application/x-mpegURL',
+                'video/avi',
+                'video/x-ms-wmv',
+                'video/x-ms-asf',
+                'video/vnd.dlna.mpeg-tts'
+            ]
+
+            if video.content_type not in valid_mime_types:
+                raise ValidationError('Unsupported file type. Please upload a valid video format.')
+
+            if video.size > 200 * 1024 * 1024:  # 200MB
+                raise ValidationError('Please keep the file size under 200MB.')
+
+        return video
 # class PropertyVideoForm(forms.ModelForm):
 #     class Meta:
 #         model = PropertyVideo
