acd0dadf16352a103d2572fd968f874245aa6ed1
Deepak kumar shah <deepakshahwal999@gmail.com>
2025-09-12 22:43:31 +0530

    update. feed

--- info/urls.py
+++ info/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import user_filter_page, property_filter_view,user_login,user_register,log_whatsapp_and_redirect, detail_view,log_call_and_redirect,mobile_login
+from .views import user_filter_by_button, user_filter_page, property_filter_view,user_login,user_register,log_whatsapp_and_redirect, detail_view,log_call_and_redirect,mobile_login
 from django.contrib import admin
 
 urlpatterns = [
-    path('', user_filter_page, name='filter'),
-    path('feed/',property_filter_view, name='feed'),
+    path('filter/', user_filter_page, name='filter'),
+    path('',property_filter_view, name='feed'),
+    path('filter/<str:filter_type>/', user_filter_by_button, name='user-filter-by-button'),
     path('login/',mobile_login, name= 'user-login'),
     path('registeration/',user_register, name='register'),
     path('video/<int:video_id>/', detail_view, name='details'),
